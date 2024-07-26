3113db3ec20bc853b1c962b0f4da73a9f0c3be2d
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-07-26 22:45:22 +0500

    View, Button, Text

---
+++ App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text style={ { fontSize : 30 } }>React Native Mobile APP Developer</Text>
+      <Text style={ { fontSize : 30 } }>Farzeen Ali</Text>
+      <Button title="Press Here" />
+    </View>
+  );
+};
+
+export default App;
