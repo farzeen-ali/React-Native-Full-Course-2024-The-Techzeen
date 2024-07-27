30b6ffa72f8521c775ccd4245376c29d1b43243f
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-07-27 21:49:05 +0500

    Components

--- App.jsx
+++ App.jsx
@@ -3,14 +3,29 @@
 
 import React from 'react';
 import { Button, Text, View } from 'react-native';
+import First from './src/components/First';
 const App = () => {
   return (
     <View>
       <Text style={ { fontSize : 30 } }>React Native Mobile APP Developer</Text>
       <Text style={ { fontSize : 30 } }>Farzeen Ali</Text>
       <Button title="Press Here" />
+      <First />
+      <First />
+      <First />
+      <First />
     </View>
   );
 };
+
+
+// const First = () => {
+//   return (
+//     <View>
+//       <Text style={ { fontSize : 30 } }>React Native</Text>
+//     </View>
+//   );
+// };
+
 
 export default App;
---
+++ src/components/First.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { Text, View } from 'react-native';
+const First = () => {
+    return (
+      <View>
+        <Text style={ { fontSize : 30 } }>React Native Tutorial</Text>
+      </View>
+    );
+  };
+
+export default First;
